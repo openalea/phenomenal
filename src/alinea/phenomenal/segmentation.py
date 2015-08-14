d5819aad5bff246427fae827155c959cae7fb4f2
Simon Artzet <simon.artzet@gmail.com>
2015-08-14 15:08:12 +0200

    Update and clean segmentation + fixed bad behavior

--- src/alinea/phenomenal/segmentation.py
+++ src/alinea/phenomenal/segmentation.py
@@ -26,127 +26,20 @@ __revision__ = ""
 #       =======================================================================
 #       External Import
 import numpy as np
-from collections import deque
+import math
+
 
 #       =======================================================================
 #       Local Import 
 
 #       =======================================================================
-#       Code
-
-def give_me_first_neighbor(skeleton_image, j, i):
-
-    neighbors = list()
-    for jj in [-1, 0, 1]:
-        for ii in [-1, 0, 1]:
-            if jj == 0 and ii == 0:
-                continue
-
-            pix = skeleton_image[j + jj, i + ii]
-            #Nothing
-            if pix == 0:
-                continue
-            #Already tagged
-            if pix < 255:
-                continue
-
-            neighbors.append((j + jj, i + ii))
-
-    return neighbors
+#       Class
 
 
-def give_me_neighbor(skeleton_image, j, i, label_number):
-
-    number = 0
-    i_next = -1
-    j_next = -1
-
-    for jj in [-1, 0, 1]:
-        for ii in [-1, 0, 1]:
-            if jj == 0 and ii == 0:
-                continue
-
-            pix = skeleton_image[j + jj, i + ii]
-
-            if pix == 0:
-                continue
-
-            if pix < label_number:
-                return -1, -1
-
-            if pix == 255:
-                number += 1
-                i_next = i + ii
-                j_next = j + jj
-
-            if number == 2:
-                return -2, -2
-
-    return j_next, i_next
-
-
-def get_closest_label(label, labels):
-
-    closest_label = list()
-
-    for r in range(20):
-        for lab in labels:
-            if lab is not label:
-                for j, i in label['index']:
-                    for jj, ii in lab['index']:
-                        if -r <= j - jj <= r and -r <= i - ii <= r:
-                            closest_label.append((lab, r))
-        if closest_label is not []:
-            break
-
-    return closest_label
-
-
-def get_first_closest_label(label, labels):
-    for r in range(20):
-        for lab in labels:
-            if lab is not label:
-                for j, i in label['index']:
-                    for jj, ii in lab['index']:
-                        if -r <= j - jj <= r and -r <= i - ii <= r:
-                            return lab, r
-
-    return None, None
-
-
-def get_closest_label(label, labels, r):
-    for lab in labels:
-        if lab is not label:
-            for j, i in label['index']:
-                for jj, ii in lab['index']:
-                    if -r <= j - jj <= r and -r <= i - ii <= r:
-                        return lab
-
-    return None
-
-
-def transfer_label(label_src, label_dest):
-    for j, i in label_src['index']:
-        label_dest['index'].append((j, i))
-
-    return label_dest
-
-
-class Segment(object):
-    def __init__(self, id_number):
-        self.id_number = id_number
-        self.first_id_number = id_number
-        self.points = list()
-        self.first_point = -1
-        self.last_point = -1
-
-    def get_size(self):
-        return len(self.points)
-
-
-class Trunk(object):
+class Organ(object):
     def __init__(self):
         self.segments = list()
+        self.id_number = None
 
     def global_position(self):
         y_mean, x_mean = (0, 0)
@@ -155,8 +48,7 @@ class Trunk(object):
                 y_mean += y
                 x_mean += x
 
-        return y_mean, x_mean
-
+        return y_mean / len(self.segments), x_mean / len(self.segments)
 
     def get_height(self):
         y_min = 5000
@@ -169,7 +61,6 @@ class Trunk(object):
 
         return y_max - y_min
 
-
     def get_width(self):
         x_min = 5000
         x_max = -5000
@@ -181,11 +72,22 @@ class Trunk(object):
 
         return x_max - x_min
 
-
-    def is_in_trunk(self, segment):
-        for trunk_segment in self.segments:
-            if trunk_segment is segment:
+    def is_in(self, segment):
+        for organ_segment in self.segments:
+            if organ_segment is segment:
                 return True
+
+        return False
+
+    def is_close(self, segment, radius=1):
+        for organ_segment in self.segments:
+            for y, x in organ_segment.points:
+                for yy, xx in segment.points:
+                    if (-radius <= y - yy <= radius and
+                        -radius <= x - xx <= radius):
+                        print y, x, yy, xx
+                        print segment.first_point, segment.last_point
+                        return True
 
         return False
 
@@ -195,29 +97,49 @@ class Trunk(object):
         print "Width : ", self.get_width()
         print "Global position : ", self.global_position()
 
-        for segment in self.segments:
-            for y, x in segment.points:
-                print y, x
+
+class Stem(Organ):
+    def __init__(self):
+        Organ.__init__(self)
 
 
-def neighbors_value(skeleton_image, y, x):
-    values = list()
+class Leaf(Organ):
+    def __init__(self):
+        Organ.__init__(self)
+
+
+class Segment(object):
+    def __init__(self, id_number, first_point):
+        self.id_number = id_number
+        self.first_id_number = id_number
+        self.points = list()
+        self.points.append(first_point)
+        self.first_point = first_point
+        self.last_point = first_point
+
+    def get_size(self):
+        return len(self.points)
+
+    def global_position(self):
+        y_mean, x_mean = (0, 0)
+        for y, x in self.points:
+            y_mean += y
+            x_mean += x
+
+        return y_mean / len(self.points), x_mean / len(self.points)
+
+#       =======================================================================
+#       Segments plants
+
+
+def neighbors_is_tagged(skeleton_image, y, x):
     for j in [-1, 0, 1]:
         for i in [-1, 0, 1]:
             if j == 0 and i == 0:
                 continue
 
-            values.append(skeleton_image[y + j, x + i])
-
-    return values
-
-
-def neighbors_is_tagged(skeleton_image, j, i):
-
-    values = neighbors_value(skeleton_image, j, i)
-    for value in values:
-        if value == -1:
-            return True
+            if skeleton_image[y + j, x + i] == -1:
+                return True
 
     return False
 
@@ -280,8 +202,11 @@ def next_neighbors(skeleton_image, y, x):
 
 
 def segment_skeleton(skeleton_image):
+
     h, l = np.shape(skeleton_image)
+
     segments = list()
+
     id_number = 1
     for j in xrange(h):
         for i in xrange(l):
@@ -301,60 +226,50 @@ def segment_skeleton(skeleton_image):
             if not (1 <= number_of_valid_neighbors <= 2):
                 continue
 
-            # Neighbors of pixel is tagged, so its considering as point junction
-            if number_of_valid_neighbors == 2:
-                if neighbors_is_tagged(skeleton_image, j, i) is True:
-                    # We create a new segments
-                    segment = Segment(id_number)
-                    segment.points.append((j, i))
-                    segment.first_point = (j, i)
-                    segment.last_point = (j, i)
-                    segments.append(segment)
-                    skeleton_image[j, i] = -1
-                    id_number += 1
-                    print id_number
-                    continue
-
             # We create a new segments
-            segment = Segment(id_number)
-            segment.points.append((j, i))
-            segment.first_point = (j, i)
-            segment.last_point = (j, i)
-            segments.append(segment)
+            segment = Segment(id_number, (j, i))
+            # We tag te pixel on skeleton image
             skeleton_image[j, i] = -1
+
+            # If possible next neighbors is 2 and furthermore 1 other pixel
+            # is tagged, is a points of junction between 3 segment so we pass
+            # the point
+            if not (number_of_valid_neighbors == 2 and
+                    neighbors_is_tagged(skeleton_image, j, i) is True):
+
+                count = 0
+                for y, x in neighbors_index:
+                    while y > -1 and x > -1:
+                        segment.points.append((y, x))
+                        skeleton_image[y, x] = -1
+                        y, x = next_neighbors(skeleton_image, y, x)
+
+                    if count == 0:
+                        segment.last_point = segment.points[-1]
+                    else:
+                        segment.first_point = segment.points[-1]
+
+                    count += 1
+
+            # Add segment to the list
+            segments.append(segment)
+            # Increment id number
             id_number += 1
-            print id_number
-
-            y, x = neighbors_index[0]
-            while y > -1 and x > -1:
-                segment.points.append((y, x))
-                segment.last_point = (y, x)
-                skeleton_image[y, x] = -1
-                y, x = next_neighbors(skeleton_image, y, x)
-
-            if number_of_valid_neighbors == 2:
-                y, x = neighbors_index[1]
-                while y > -1 and x > -1:
-                    segment.points.append((y, x))
-                    segment.first_point = (y, x)
-                    skeleton_image[y, x] = -1
-                    y, x = next_neighbors(skeleton_image, y, x)
 
     return segments
 
+#       =======================================================================
+#       Segment Stem
 
-def segment_organs_skeleton_image(skeleton_image):
 
-    skeleton_image = skeleton_image.astype(np.int)
+def get_possible_stem_segment(segments):
 
-    segments = segment_skeleton(skeleton_image)
-
-    candidates_trunc = list()
+    possible_stem_segment = list()
     for segment in segments:
         size_segments = len(segment.points)
 
         if size_segments <= 4:
-            candidates_trunc.append(segment)
+            possible_stem_segment.append(segment)
             continue
 
         yb, xb = segment.first_point
@@ -362,13 +277,11 @@ def segment_organs_skeleton_image(skeleton_image):
         yo, xo = (yb, xa)
 
         if xa == xb:
-            candidates_trunc.append(segment)
+            possible_stem_segment.append(segment)
             continue
 
         if ya == yb:
             continue
-
-        import math
 
         AB = math.sqrt((xa - xb)**2 + (ya - yb)**2)
         AO = math.sqrt((xa - xo)**2 + (ya - yo)**2)
@@ -378,48 +291,134 @@ def segment_organs_skeleton_image(skeleton_image):
         alpha = alpha * 180.0 / np.pi
 
         if 0 <= alpha <= 25:
-            candidates_trunc.append(segment)
+            possible_stem_segment.append(segment)
             continue
 
-    r = 5
-    for segment_1 in candidates_trunc:
-        for segment_2 in candidates_trunc:
-            if segment_2.id_number != segment_1.id_number:
-                for j, i in segment_1.points:
-                    for jj, ii in segment_2.points:
-                        if -r <= j - jj <= r and -r <= i - ii <= r:
-                            segment_2.id_number = segment_1.id_number
+    print "Candidates stem : ", len(possible_stem_segment)
+
+    return possible_stem_segment
 
 
-    trunks = dict()
-    for segment in candidates_trunc:
-        if segment.id_number not in trunks:
-            trunks[segment.id_number] = Trunk()
+def build_stem(possible_stem_segment):
+    radius = 5
+    stems = list()
+    while possible_stem_segment:
+        segment = possible_stem_segment.pop()
 
-        trunks[segment.id_number].segments.append(segment)
+        stem = Stem()
+        stem.segments.append(segment)
+        stem.id_number = segment.id_number
+        stems.append(stem)
 
+        stop = False
+        while stop is False:
+            stop = True
+            for seg_left in possible_stem_segment[:]:
+                if stem.is_close(seg_left, radius):
+                    stem.segments.append(seg_left)
+                    possible_stem_segment.remove(seg_left)
+                    stop = False
 
-    final_trunk = None
+    stem_choose = None
     max_height = 0
-    for id in trunks.keys():
-        height = trunks[id].get_height()
+    for stem in stems:
+        height = stem.get_height()
         if height > max_height:
             max_height = height
-            final_trunk = trunks[id]
+            stem_choose = stem
+
+    return stem_choose
 
 
-    final_trunk.print_value()
+def segment_stem(segments):
 
-    for segment in candidates_trunc:
-        if final_trunk.is_in_trunk(segment) is False:
-            segment.id_number = segment.first_id_number
+    possible_stem_segment = get_possible_stem_segment(segments)
+
+    stem = build_stem(possible_stem_segment)
+
+    return stem
+
+#       =======================================================================
+#       Segment Leaves
 
 
+def segment_leaves(segments, stem):
 
+    leaves = list()
+
+    def get_first_organs():
+        for segment in segments:
+            if stem.is_close(segment):
+                return segment
+        return None
+
+    while True:
+        seg = get_first_organs()
+
+        if seg is None:
+            break
+        else:
+            segments.remove(seg)
+
+            my_leaf = Leaf()
+            my_leaf.segments.append(seg)
+            my_leaf.id_number = seg.id_number
+
+            stop = False
+            while stop is False:
+                stop = True
+                for seg_left in segments[:]:
+
+                    if my_leaf.is_close(seg_left, 2):
+                        my_leaf.segments.append(seg_left)
+                        segments.remove(seg_left)
+                        stop = False
+
+            leaves.append(my_leaf)
+
+    print "Number of leaf : ", len(leaves)
+
+    return leaves, segments
+
+#       =======================================================================
+#       Segment Organs
+
+def segment_organs_skeleton_image(skeleton_image):
+
+    # Transform skeleton image as type int for tag pixel to -1
+    skeleton_image = skeleton_image.astype(np.int)
+
+    # Get the segments list of the skeleton image
+    segments = segment_skeleton(skeleton_image)
+
+    # Get the stem segments
+    stem = segment_stem(segments)
+
+    # Remove stem segment from the segments list
+    segments[:] = [segment
+                   for segment in segments
+                   if not stem.is_in(segment) is True]
+
+    leaves, segments = segment_leaves(segments, stem)
+
+
+#   ============================================================================
+
+    # Write if number on pixel image:
     for segment in segments:
         for y, x in segment.points:
             skeleton_image[y, x] = segment.id_number
 
+    for segment in stem.segments:
+        for y, x in segment.points:
+            skeleton_image[y, x] = 255
+
+    for leaf in leaves:
+        for segment in leaf.segments:
+            for y, x in segment.points:
+                skeleton_image[y, x] = leaf.id_number
+
+    skeleton_image = skeleton_image.astype(np.uint8)
     return skeleton_image
 
 
