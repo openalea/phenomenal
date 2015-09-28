9f7ea41bef96a649786eb8affd6b3d8a942b4d5a
Simon Artzet <simon.artzet@gmail.com>
2015-09-28 12:03:17 +0200

    modification of configuration loaded Add test of configuration Mdofication of load configuration in binarize test and example

--- test/test_binarization_hsv.py
+++ test/test_binarization_hsv.py
@@ -32,112 +32,119 @@ import alinea.phenomenal.result_viewer
 
 
 def test_wrong_parameters_1():
-    load_configuration = alinea.phenomenal.configuration.loadconfig(
-        'configuration_side_image_michael.cfg')
-    configuration_side = alinea.phenomenal.configuration.binarization_config(
-        load_configuration)
+    factor_side_binarization = \
+        alinea.phenomenal.configuration.binarization_factor(
+            'configuration_side_image_michael.cfg')
 
-    result = alinea.phenomenal.binarization.binarization(None,
-                                                         configuration_side,
-                                                         methods='hsv',
-                                                         is_top_image=False,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        None,
+        factor_side_binarization,
+        methods='hsv',
+        is_top_image=False,
+        mean_image=None)
     assert result is None
 
     image = numpy.zeros((2056, 2056))
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         None,
-                                                         methods='hsv',
-                                                         is_top_image=False,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        None,
+        methods='hsv',
+        is_top_image=False,
+        mean_image=None)
     assert result is None
 
     image = numpy.zeros((2056, 2056))
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         configuration_side,
-                                                         methods='hsv',
-                                                         is_top_image=False,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        factor_side_binarization,
+        methods='hsv',
+        is_top_image=False,
+        mean_image=None)
     assert result is None
 
     image = numpy.zeros((2454, 2056))
     mean_image = numpy.ones((2454, 2056))
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         configuration_side,
-                                                         methods='hsv',
-                                                         is_top_image=False,
-                                                         mean_image=mean_image)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        factor_side_binarization,
+        methods='hsv',
+        is_top_image=False,
+        mean_image=mean_image)
     assert result is None
 
 
 def test_wrong_parameters_2():
 
-    load_configuration = alinea.phenomenal.configuration.loadconfig(
-        'configuration_top_image.cfg')
-    configuration_top = alinea.phenomenal.configuration.binarization_config(
-        load_configuration)
+    factor_top_binarization = \
+        alinea.phenomenal.configuration.binarization_factor(
+            'configuration_top_image.cfg')
 
-    result = alinea.phenomenal.binarization.binarization(None,
-                                                         configuration_top,
-                                                         methods='hsv',
-                                                         is_top_image=True,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        None,
+        factor_top_binarization,
+        methods='hsv',
+        is_top_image=True,
+        mean_image=None)
     assert result is None
 
     image = numpy.zeros((2056, 2056))
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         None,
-                                                         methods='hsv',
-                                                         is_top_image=True,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        None,
+        methods='hsv',
+        is_top_image=True,
+        mean_image=None)
     assert result is None
 
     image = numpy.zeros((2056, 2056))
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         configuration_top,
-                                                         methods='hsv',
-                                                         is_top_image=True,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        factor_top_binarization,
+        methods='hsv',
+        is_top_image=True,
+        mean_image=None)
     assert result is None
 
     image = numpy.zeros((2454, 2056))
-    mean_image = numpy.ones((2454, 2056))
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         configuration_top,
-                                                         methods='hsv',
-                                                         is_top_image=True,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        factor_top_binarization,
+        methods='hsv',
+        is_top_image=True,
+        mean_image=None)
     assert result is None
 
 
 def test_binarization_hsv_1():
-    load_configuration = alinea.phenomenal.configuration.loadconfig(
-        'configuration_side_image_michael.cfg')
-    configuration_side = alinea.phenomenal.configuration.binarization_config(
-        load_configuration)
+    factor_side_binarization = \
+        alinea.phenomenal.configuration.binarization_factor(
+            'configuration_side_image_michael.cfg')
 
     image = numpy.zeros((2454, 2056, 3), dtype=numpy.uint8)
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         configuration_side,
-                                                         methods='hsv',
-                                                         is_top_image=False,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        factor_side_binarization,
+        methods='hsv',
+        is_top_image=False,
+        mean_image=None)
+
     assert (result == 0).all()
     assert len(numpy.shape(result)) == 2
 
 
 def test_binarization_hsv_2():
-    load_configuration = alinea.phenomenal.configuration.loadconfig(
-        'configuration_top_image.cfg')
-    configuration_top = alinea.phenomenal.configuration.binarization_config(
-        load_configuration)
+    factor_top_binarization = \
+        alinea.phenomenal.configuration.binarization_factor(
+            'configuration_top_image.cfg')
 
     image = numpy.zeros((2454, 2056, 3), dtype=numpy.uint8)
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         configuration_top,
-                                                         methods='hsv',
-                                                         is_top_image=True,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        factor_top_binarization,
+        methods='hsv',
+        is_top_image=True,
+        mean_image=None)
+
     assert (result == 0).all()
     assert len(numpy.shape(result)) == 2
 
--- test/test_binarization_mean_shift.py
+++ test/test_binarization_mean_shift.py
@@ -27,65 +27,67 @@ import alinea.phenomenal.configuration
 import alinea.phenomenal.load_data
 import alinea.phenomenal.result_viewer
 
+
 #       ========================================================================
 #       Code
 
 
 def test_wrong_parameters():
-    load_configuration = alinea.phenomenal.configuration.loadconfig(
-        'configuration_image_basic.cfg')
+    factor_side_binarization = \
+        alinea.phenomenal.configuration.binarization_factor(
+            'configuration_image_basic.cfg')
 
-    configuration_side = alinea.phenomenal.configuration.binarization_config(
-        load_configuration)
-
-    result = alinea.phenomenal.binarization.binarization(None,
-                                                         configuration_side,
-                                                         methods='mean_shift',
-                                                         is_top_image=False,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        None,
+        factor_side_binarization,
+        methods='mean_shift',
+        is_top_image=False,
+        mean_image=None)
     assert result is None
 
     image = numpy.zeros((2056, 2056))
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         None,
-                                                         methods='mean_shift',
-                                                         is_top_image=False,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        None,
+        methods='mean_shift',
+        is_top_image=False,
+        mean_image=None)
     assert result is None
 
     image = numpy.zeros((2056, 2056))
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         configuration_side,
-                                                         methods='mean_shift',
-                                                         is_top_image=False,
-                                                         mean_image=None)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        factor_side_binarization,
+        methods='mean_shift',
+        is_top_image=False,
+        mean_image=None)
     assert result is None
 
     image = numpy.zeros((2454, 2056))
     mean_image = numpy.ones((2454, 2056))
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         configuration_side,
-                                                         methods='mean_shift',
-                                                         is_top_image=False,
-                                                         mean_image=mean_image)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        factor_side_binarization,
+        methods='mean_shift',
+        is_top_image=False,
+        mean_image=mean_image)
     assert result is None
 
 
 def test_binarization_mean_shift():
-
-    load_configuration = alinea.phenomenal.configuration.loadconfig(
-        'configuration_image_basic.cfg')
-
-    configuration_side = alinea.phenomenal.configuration.binarization_config(
-        load_configuration)
+    factor_side_binarization = \
+        alinea.phenomenal.configuration.binarization_factor(
+            'configuration_image_basic.cfg')
 
     image = numpy.ones((2454, 2056, 3), dtype=numpy.uint8)
     mean_image = numpy.ones((2454, 2056, 3), dtype=numpy.uint8)
-    result = alinea.phenomenal.binarization.binarization(image,
-                                                         configuration_side,
-                                                         methods='mean_shift',
-                                                         is_top_image=False,
-                                                         mean_image=mean_image)
+    result = alinea.phenomenal.binarization.binarization(
+        image,
+        factor_side_binarization,
+        methods='mean_shift',
+        is_top_image=False,
+        mean_image=mean_image)
+
     assert (result == 0).all()
     assert len(numpy.shape(result)) == 2
 
