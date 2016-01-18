33f939dd664466ec5b3ef03e54e6282a1dd7980a
Simon Artzet <simon.artzet@gmail.com>
2016-01-18 17:31:26 +0100

    delete deprecated function and delete useles mask

--- test/test_binarization_side_binarization_hsv.py
+++ test/test_binarization_side_binarization_hsv.py
@@ -15,102 +15,39 @@
 # ==============================================================================
 import numpy
 
-from alinea.phenomenal.plant_1 import plant_1_images
-from alinea.phenomenal.binarization import (side_binarization_hsv,
-                                            side_binarization_routine_hsv)
-from alinea.phenomenal.configuration import binarization_factor
+from alinea.phenomenal.plant_1 import (plant_1_images,
+                                       plant_1_mask_hsv_roi_main,
+                                       plant_1_mask_hsv_roi_band,
+                                       plant_1_mask_hsv_roi_pot,
+                                       plant_1_background_hsv)
+
+from alinea.phenomenal.binarization import side_binarization_routine_hsv
 # ==============================================================================
 
 
-def test_wrong_parameters_1():
-
-    factor_side_binarization = binarization_factor('factor_image_basic.cfg')
-
-    try:
-        side_binarization_hsv(None, factor_side_binarization)
-    except Exception, e:
-        assert e.message == 'image should be a numpy.ndarray'
-        assert type(e) == TypeError
-    else:
-        assert False
-
-
-def test_wrong_parameters_2():
-
-    image = numpy.zeros((25, 25, 3), dtype=numpy.uint8)
-
-    try:
-        side_binarization_hsv(image, None)
-    except Exception, e:
-        assert e.message == 'factor should be a BinarizationFactor object'
-        assert type(e) == TypeError
-    else:
-        assert False
-
-
-def test_wrong_parameters_3():
-
-    factor_side_binarization = binarization_factor('factor_image_basic.cfg')
-    image = numpy.zeros((25, 25), dtype=numpy.uint8)
-
-    try:
-        side_binarization_hsv(image, factor_side_binarization)
-    except Exception, e:
-        assert e.message == 'image should be 3D array'
-        assert type(e) == ValueError
-    else:
-        print False
-
-
-def test_side_binarization_hsv_1():
-
-    factor = binarization_factor('factor_image_basic.cfg')
-    image = numpy.zeros((2454, 2056, 3), dtype=numpy.uint8)
-
-    binarize_image = side_binarization_hsv(image, factor)
-
-    assert (binarize_image == 0).all()
-    assert binarize_image.ndim == 2
-    assert binarize_image.shape == (2454, 2056)
+# def test_side_binarization_hsv_1():
+#
+#     factor = binarization_factor('factor_image_basic.cfg')
+#     image = numpy.zeros((2454, 2056, 3), dtype=numpy.uint8)
+#
+#     binarize_image = side_binarization_hsv(image, factor)
+#
+#     assert (binarize_image == 0).all()
+#     assert binarize_image.ndim == 2
+#     assert binarize_image.shape == (2454, 2056)
 
 
 def test_no_regression_1():
-    factor = binarization_factor('factor_image_basic.cfg')
 
-    images = plant_1_images()
-    images.pop(-1)
-
-    refs = [(0, 121726),
-            (30, 104465),
-            (60, 141807),
-            (90, 126528),
-            (120, 114964),
-            (150, 118759),
-            (180, 118949),
-            (210, 83357),
-            (240, 139211),
-            (270, 120737),
-            (300, 108052),
-            (330, 113243)]
-
-    for angle, ref in refs:
-        image_0_binarize = side_binarization_hsv(images[angle], factor)
-
-        assert numpy.count_nonzero(image_0_binarize) == ref
-
-
-def test_no_regression_2():
-    factor = binarization_factor('factor_image_basic.cfg')
-
-    cubicle_domain = factor.side_cubicle_domain
-    cubicle_background = factor.side_cubicle_background
-    main_hsv_min = factor.side_roi_main.hsv_min
-    main_hsv_max = factor.side_roi_main.hsv_max
-    main_mask = factor.side_roi_main.mask
-    orange_band_mask = factor.side_roi_orange_band.mask
-    pot_hsv_min = factor.side_roi_pot.hsv_min
-    pot_hsv_max = factor.side_roi_pot.hsv_max
-    pot_mask = factor.side_roi_pot.mask
+    cubicle_domain = (0, 2190, 275, 1746)
+    cubicle_background = plant_1_background_hsv()
+    main_hsv_min = (30, 11, 0)
+    main_hsv_max = (129, 254, 141)
+    main_mask = plant_1_mask_hsv_roi_main()
+    orange_band_mask = plant_1_mask_hsv_roi_band()
+    pot_hsv_min = (14, 36, 0)
+    pot_hsv_max = (88, 254, 88)
+    pot_mask = plant_1_mask_hsv_roi_pot()
 
     images = plant_1_images()
     images.pop(-1)
@@ -147,11 +84,6 @@ def test_no_regression_2():
 # ==============================================================================
 
 if __name__ == "__main__":
-    test_wrong_parameters_1()
-    test_wrong_parameters_2()
-    test_wrong_parameters_3()
-
-    test_side_binarization_hsv_1()
+    # test_side_binarization_hsv_1()
 
     test_no_regression_1()
-    test_no_regression_2()
