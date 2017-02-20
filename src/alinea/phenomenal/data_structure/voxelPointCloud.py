e1dd94c83f685a6c244c2989b7f14acd040a754f
Simon Artzet <simon.artzet@gmail.com>
2017-02-20 14:39:47 +0100

    save format

--- src/alinea/phenomenal/data_structure/voxelPointCloud.py
+++ src/alinea/phenomenal/data_structure/voxelPointCloud.py
@@ -13,6 +13,8 @@ import os
 import re
 import json
 import numpy
+import csv
+from ast import literal_eval
 
 from alinea.phenomenal.data_structure.image3d import Image3D
 from alinea.phenomenal.data_structure.voxelGraph import VoxelGraph
@@ -118,3 +120,20 @@ class VoxelPointCloud(object):
         f.close()
 
         return VoxelPointCloud(voxels_position, voxels_size)
+
+    @staticmethod
+    def read_from_csv(filename):
+
+        with open(filename, 'rb') as f:
+            reader = csv.reader(f)
+
+            next(reader)
+
+            voxels_position = list()
+            for number_id, position, size in reader:
+                position = literal_eval(position)
+                voxels_position.append(position)
+
+            voxels_size
+
+            return VoxelPointCloud(voxels_position, voxels_size)
