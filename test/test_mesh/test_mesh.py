4c0feb451e0aa68066fcac1aec6ce344b8dd0b58
Simon Artzet <simon.artzet@gmail.com>
2017-02-20 13:51:46 +0100

    Refactoring

--- test/test_mesh/test_mesh.py
+++ test/test_mesh/test_mesh.py
@@ -10,7 +10,7 @@
 #
 # ==============================================================================
 from alinea.phenomenal.data_structure import (
-    voxel_centers_to_image_3d)
+    voxels_position_to_image_3d)
 
 from alinea.phenomenal.mesh.algorithms import meshing
 from alinea.phenomenal.data_access.plant_1 import plant_1_voxel_centers
@@ -19,10 +19,11 @@ from alinea.phenomenal.data_access.plant_1 import plant_1_voxel_centers
 
 def test_mesh_empty_voxel_centers():
 
-    voxel_size = 8
-    voxel_centers = list()
+    voxels_size = 8
+    voxels_position = list()
     try:
-        image_3d = voxel_centers_to_image_3d(voxel_centers, voxel_size)
+        image_3d = voxels_position_to_image_3d(voxels_position, voxels_size)
+
     except Exception, e:
         assert type(e) == ValueError
     else:
@@ -30,13 +31,14 @@ def test_mesh_empty_voxel_centers():
 
 
 def test_mesh_one_voxel_centers():
-    voxel_size = 1
-    voxel_centers = list()
-    voxel_centers.append((0, 0, 0))
+    voxels_size = 1
+    voxels_position = list()
+    voxels_position.append((0, 0, 0))
 
     try:
-        image_3d = voxel_centers_to_image_3d(voxel_centers, voxel_size)
+        image_3d = voxels_position_to_image_3d(voxels_position, voxels_size)
         vertices, faces = meshing(image_3d)
+
     except Exception, e:
         assert type(e) == ValueError
     else:
@@ -44,31 +46,27 @@ def test_mesh_one_voxel_centers():
 
 
 def test_mesh_two_voxel_center():
-    voxel_size = 4
-    voxel_centers = list()
-    voxel_centers.append((0, 0, 0))
-    voxel_centers.append((4, 4, 8))
-    voxel_centers.append((8, 8, 8))
+    voxels_size = 4
+    voxels_position = list()
+    voxels_position.append((0, 0, 0))
+    voxels_position.append((4, 4, 8))
+    voxels_position.append((8, 8, 8))
 
-    # image_3d = voxel_centers_to_image_3d(voxel_centers, voxel_size)
-    #
+    # image_3d = voxels_position_to_image_3d(voxel_centers, voxel_size)
     # print image_3d.size
-    #
     # vertices, faces = meshing(image_3d)
 
 
 def test_mesh_normal():
-    voxel_size = 20
-    voxel_centers = plant_1_voxel_centers(voxel_size=voxel_size)
+    voxels_size = 20
+    voxels_position = plant_1_voxel_centers(voxel_size=voxels_size)
 
-    image_3d = voxel_centers_to_image_3d(voxel_centers, voxel_size)
+    image_3d = voxels_position_to_image_3d(voxels_position, voxels_size)
 
     vertices, faces = meshing(image_3d,
                               smoothing_iteration=2,
                               reduction=0.95,
                               verbose=True)
-
-    print len(vertices), len(faces)
 
     assert 100 <= len(vertices) <= 273
     assert 200 <= len(faces) <= 542
