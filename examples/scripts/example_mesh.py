ff84db4e6574524d194eb8470a7c793fd6399839
Simon Artzet <simon.artzet@gmail.com>
2017-07-28 16:16:59 +0200

    test gitlab-ci with cache

--- examples/scripts/example_mesh.py
+++ examples/scripts/example_mesh.py
@@ -18,8 +18,7 @@ from alinea.phenomenal.data_access.plant_1 import (
 
 from alinea.phenomenal.mesh import meshing
 from alinea.phenomenal.display import (
-    show_voxel_point_cloud,
-    show_mesh)
+    DisplayVoxelPointCloud)
 
 # ==============================================================================
 
@@ -28,13 +27,15 @@ def main():
 
     vpc = plant_1_voxels_size_4_without_loss_120()
 
-    show_voxel_point_cloud(vpc,
-                           # size=(5000, 5000),
-                           color=(0.1, 0.9, 0.1),
-                           azimuth=310,
-                           distance=3000,
-                           elevation=90,
-                           focalpoint=(0, 0, 0))
+    DisplayVoxelPointCloud().show(vpc)
+
+    # show_voxel_point_cloud(vpc,
+    #                        # size=(5000, 5000),
+    #                        color=(0.1, 0.9, 0.1),
+    #                        azimuth=310,
+    #                        distance=3000,
+    #                        elevation=90,
+    #                        focalpoint=(0, 0, 0))
 
     image_3d = vpc.to_image_3d()
 
@@ -42,6 +43,10 @@ def main():
                               reduction=1,
                               smoothing_iteration=10,
                               verbose=True)
+    import alinea.phenomenal.mesh.formats as f
+
+    f.write_vertices_faces_to_ply_file('test.ply', vertices, list())
+
 
     print("Number of vertices : {nb_vertices}".format(
         nb_vertices=len(vertices)))
@@ -49,14 +54,14 @@ def main():
     print("Number of faces : {nb_faces}".format(
         nb_faces=len(faces)))
 
-    show_mesh(vertices, faces,
-              representation="surface",
-              size=(5000, 5000),
-              color=(0.1, 0.9, 0.1),
-              azimuth=120,
-              distance=3000,
-              elevation=90,
-              focalpoint=(0, 0, 0))
+    # show_mesh(vertices, faces,
+    #           representation="surface",
+    #           size=(5000, 5000),
+    #           color=(0.1, 0.9, 0.1),
+    #           azimuth=120,
+    #           distance=3000,
+    #           elevation=90,
+    #           focalpoint=(0, 0, 0))
 
 if __name__ == "__main__":
     main()
