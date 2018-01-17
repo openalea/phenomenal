7aa56897acedae3bbf8e22342f920b8745db0eac
gaetan heidsieck <gaetan.heidsieck@inria.fr>
2018-01-17 18:39:38 +0100

    Add automatic generation of wraleas

---
+++ src/openalea/phenomenal_wralea/mesh/__wralea__.py
@@ -0,0 +1,258 @@
+
+# This file has been generated at Wed Jan 17 18:30:12 2018
+
+from openalea.core import *
+
+
+__name__ = 'openalea.phenomenal.mesh'
+
+__editable__ = True
+__version__ = '1.6.0'
+__license__ = 'CeCILL-C'
+__authors__ = 'Me'
+
+
+__all__ = ['openalea.phenomenal.mesh_read_json_file_to_vertices_faces', 'openalea.phenomenal.mesh_decimation', 'openalea.phenomenal.mesh_marching_cubes', 'openalea.phenomenal.mesh_smoothing', 'openalea.phenomenal.mesh_write_vertices_faces_to_ply_file', 'openalea.phenomenal.mesh_voxelization', 'openalea.phenomenal.mesh_from_voxel_centers_to_vtk_image_data', 'openalea.phenomenal.mesh_centers', 'openalea.phenomenal.mesh_meshing', 'openalea.phenomenal.mesh_normals', 'openalea.phenomenal.mesh_write_vertices_faces_to_json_file', 'openalea.phenomenal.mesh_from_vtk_image_data_to_voxels_center', 'openalea.phenomenal.mesh_write_vtk_poly_data_to_ply_file', 'openalea.phenomenal.mesh_from_numpy_matrix_to_vtk_image_data', 'openalea.phenomenal.mesh_from_vertices_faces_to_vtk_poly_data', 'openalea.phenomenal.mesh_from_vtk_poly_data_to_vertices_faces']
+
+
+
+openalea_phenomenal_mesh_read_json_file_to_vertices_faces = Factory(name='read_json_file_to_vertices_faces',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='read_json_file_to_vertices_faces',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_decimation = Factory(name='decimation',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='decimation',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_marching_cubes = Factory(name='marching_cubes',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='marching_cubes',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_smoothing = Factory(name='smoothing',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='smoothing',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_write_vertices_faces_to_ply_file = Factory(name='write_vertices_faces_to_ply_file',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='write_vertices_faces_to_ply_file',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_voxelization = Factory(name='voxelization',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='voxelization',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_from_voxel_centers_to_vtk_image_data = Factory(name='from_voxel_centers_to_vtk_image_data',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='from_voxel_centers_to_vtk_image_data',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_centers = Factory(name='centers',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='centers',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_meshing = Factory(name='meshing',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='meshing',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_normals = Factory(name='normals',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='normals',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_write_vertices_faces_to_json_file = Factory(name='write_vertices_faces_to_json_file',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='write_vertices_faces_to_json_file',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_from_vtk_image_data_to_voxels_center = Factory(name='from_vtk_image_data_to_voxels_center',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='from_vtk_image_data_to_voxels_center',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_write_vtk_poly_data_to_ply_file = Factory(name='write_vtk_poly_data_to_ply_file',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='write_vtk_poly_data_to_ply_file',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_from_numpy_matrix_to_vtk_image_data = Factory(name='from_numpy_matrix_to_vtk_image_data',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='from_numpy_matrix_to_vtk_image_data',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_from_vertices_faces_to_vtk_poly_data = Factory(name='from_vertices_faces_to_vtk_poly_data',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='from_vertices_faces_to_vtk_poly_data',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_mesh_from_vtk_poly_data_to_vertices_faces = Factory(name='from_vtk_poly_data_to_vertices_faces',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.mesh',
+                nodemodule='openalea.phenomenal.mesh',
+                nodeclass='from_vtk_poly_data_to_vertices_faces',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
---
+++ src/openalea/phenomenal_wralea/object/__wralea__.py
@@ -0,0 +1,153 @@
+
+# This file has been generated at Wed Jan 17 18:30:12 2018
+
+from openalea.core import *
+
+
+__name__ = 'openalea.phenomenal.object'
+
+__editable__ = True
+__version__ = '1.6.0'
+__license__ = 'CeCILL-C'
+__authors__ = 'Me'
+
+
+__all__ = ['openalea.phenomenal.object_VoxelSegment', 'openalea.phenomenal.object_VoxelGrid', 'openalea.phenomenal.object_ImageView', 'openalea.phenomenal.object_VoxelSkeleton', 'openalea.phenomenal.object_Image3D', 'openalea.phenomenal.object_VoxelOctree', 'openalea.phenomenal.object_VoxelOrgan', 'openalea.phenomenal.object_VoxelGraph', 'openalea.phenomenal.object_VoxelSegmentation']
+
+
+
+openalea_phenomenal_object_VoxelSegment = Factory(name='VoxelSegment',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.object',
+                nodemodule='openalea.phenomenal.object',
+                nodeclass='VoxelSegment',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_object_VoxelGrid = Factory(name='VoxelGrid',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.object',
+                nodemodule='openalea.phenomenal.object',
+                nodeclass='VoxelGrid',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_object_ImageView = Factory(name='ImageView',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.object',
+                nodemodule='openalea.phenomenal.object',
+                nodeclass='ImageView',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_object_VoxelSkeleton = Factory(name='VoxelSkeleton',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.object',
+                nodemodule='openalea.phenomenal.object',
+                nodeclass='VoxelSkeleton',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_object_Image3D = Factory(name='Image3D',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.object',
+                nodemodule='openalea.phenomenal.object',
+                nodeclass='Image3D',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_object_VoxelOctree = Factory(name='VoxelOctree',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.object',
+                nodemodule='openalea.phenomenal.object',
+                nodeclass='VoxelOctree',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_object_VoxelOrgan = Factory(name='VoxelOrgan',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.object',
+                nodemodule='openalea.phenomenal.object',
+                nodeclass='VoxelOrgan',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_object_VoxelGraph = Factory(name='VoxelGraph',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.object',
+                nodemodule='openalea.phenomenal.object',
+                nodeclass='VoxelGraph',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+openalea_phenomenal_object_VoxelSegmentation = Factory(name='VoxelSegmentation',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.object',
+                nodemodule='openalea.phenomenal.object',
+                nodeclass='VoxelSegmentation',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
