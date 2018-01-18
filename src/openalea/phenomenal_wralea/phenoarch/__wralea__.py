699813967e4040bcf48b105ad57cb648e6edeb30
gaetan heidsieck <gaetan.heidsieck@inria.fr>
2018-01-18 16:37:53 +0100

    add functions binarization

---
+++ src/openalea/phenomenal_wralea/phenoarch/__wralea__.py
@@ -0,0 +1,54 @@
+
+# This file has been generated at Wed Jan 17 18:30:12 2018
+
+from openalea.core import *
+
+
+__name__ = 'openalea.phenomenal.phenoarch'
+
+__editable__ = True
+__version__ = '1.6.0'
+__license__ = 'CeCILL-C'
+__authors__ = 'Me'
+
+
+__all__ = ['openalea_phenomenal_phenoarch_routine_side_binarization', 'openalea_phenomenal_phenoarch_routine_top_binarization', 'openalea_phenomenal_phenoarch_binarize']
+
+
+
+openalea_phenomenal_phenoarch_routine_side_binarization = Factory(name='routine_side_binarization',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.phenoarch',
+                nodemodule='openalea.phenomenal.phenoarch',
+                nodeclass='routine_side_binarization',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+openalea_phenomenal_phenoarch_routine_top_binarization = Factory(name='routine_top_binarization',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.phenoarch',
+                nodemodule='openalea.phenomenal.phenoarch',
+                nodeclass='routine_top_binarization',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+openalea_phenomenal_phenoarch_binarize = Factory(name='binarize',
+                authors='Me (wralea authors)',
+                description='',
+                category='openalea.phenomenal.phenoarch',
+                nodemodule='openalea.phenomenal.phenoarch',
+                nodeclass='binarize',
+                inputs=None,
+                outputs=None,
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
