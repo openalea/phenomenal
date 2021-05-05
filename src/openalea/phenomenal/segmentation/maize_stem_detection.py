672ae36a37e941cdb51da683f93122e45addfb5f
BenoitDaviet <benoit.daviet@inrae.fr>
2021-05-05 16:49:58 +0200

    setup algo

--- src/openalea/phenomenal/segmentation/maize_stem_detection.py
+++ src/openalea/phenomenal/segmentation/maize_stem_detection.py
@@ -54,7 +54,7 @@ def get_nodes_radius(center, points, radius):
 
 
 def stem_detection(stem_segment_voxel, stem_segment_path, voxels_size,
-                   graph, distance_plane=1.00):
+                   graph, z_stem=None, distance_plane=1.00):
 
     # ==========================================================================
 
@@ -92,6 +92,12 @@ def stem_detection(stem_segment_voxel, stem_segment_path, voxels_size,
 
     nodes_length = list(map(float, map(len, arr_closest_nodes_planes)))
     min_peaks_stem = maize_stem_peak_detection(nodes_length, stop_index)
+
+
+    if z_stem is not None:
+        list_z = [numpy.mean(plane[:, 2]) for plane in arr_closest_nodes_planes]
+        index_z = numpy.argmin(abs(numpy.array(list_z) - z_stem))
+        min_peaks_stem = [tup for tup in min_peaks_stem if tup[0] < index_z] + [(index_z, 0.)]
 
     window_length = max(4, len(nodes_length) // 8)
     window_length = window_length + 1 if window_length % 2 == 0 else window_length
