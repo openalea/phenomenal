b623182f107a98d5ec573e3a1d0aa1c1c768d780
Aurelien Besnier <aurelien.besnier@inrae.fr>
2024-01-25 13:20:35 +0100

    Reusing setup.py for now

--- setup.py
+++ setup.py
@@ -18,8 +18,16 @@
 # ==============================================================================
 import numpy
 from Cython.Build import cythonize
-from setuptools import setup, Extension
+from setuptools import setup, find_namespace_packages, Extension
 # ==============================================================================
+
+namespace = "openalea"
+pkg_root_dir = 'src'
+packages = find_namespace_packages(where='src', include=['openalea', 'openalea.*'])
+top_pkgs = [pkg for pkg in packages if len(pkg.split('.')) <= 2]
+package_dir = dict([('', pkg_root_dir)] +
+                   [(pkg, pkg_root_dir + "/" + pkg.replace('.', '/'))
+                    for pkg in top_pkgs])
 
 
 extentions = [
@@ -37,10 +45,35 @@ extentions = [
 
 setup(
     name="openalea.phenomenal",
+    version="1.9.1",
+    description="",
+    long_description="",
+
+    author="* Simon Artzet\n"
+           "* Christian Fournier\n"
+           "* Mielewczik Michael\n"
+           "* Brichet Nicolas\n"
+           "* Chopard Jerome\n"
+           "* Christophe Pradal\n",
+
+    author_email="simon.artzet@gmail.com",
+    maintainer="Simon Artzet",
+    maintainer_email="simon.artzet@gmail.com",
+
+    url="https://github.com/openalea/phenomenal",
+    license="Cecill-C",
+    keywords='',
+
+    # package installation
+    packages=packages,
+    package_dir=package_dir,
+    zip_safe=False,
     ext_modules=cythonize(extentions),
+
     entry_points={
         "wralea": ["openalea.phenomenal = openalea.phenomenal_wralea", ],
     },
+
     # See MANIFEST.in
     include_package_data=True,
     )
