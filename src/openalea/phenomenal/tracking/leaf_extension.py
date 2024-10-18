2642dc55229d67c347552b81d66df940b6ee3572
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2024-10-18 14:56:25 +0200

    Updating skan fcts to 0.11

--- src/openalea/phenomenal/tracking/leaf_extension.py
+++ src/openalea/phenomenal/tracking/leaf_extension.py
@@ -35,7 +35,7 @@ def skeleton_branches(image, n_kernel=15, min_length=30):
 
     binary = image.copy()
     if round(np.max(binary)) == 255:
-        binary = binary / 255.
+        binary = binary / 255.0
 
     # dilate image
     kernel = np.ones((n_kernel, n_kernel))
@@ -46,22 +46,26 @@ def skeleton_branches(image, n_kernel=15, min_length=30):
 
     # skeleton analysis : get branches
     skan_skeleton = Skeleton(skeleton)
-    branches = summarize(skan_skeleton, separator='-')
+    branches = summarize(skan_skeleton)
 
     # select branches having an endpoint, and a sufficient length
-    branches_endpoint = branches[branches['branch-type'] == 1]
-    branches_endpoint = branches_endpoint[branches_endpoint['branch-distance'] > min_length]
+    branches_endpoint = branches[branches["branch-type"] == 1]
+    branches_endpoint = branches_endpoint[
+        branches_endpoint["branch-distance"] > min_length
+    ]
 
     # converting branches to polylines
     _, coordinates = skeleton_to_csgraph(skeleton)
-    node_ids = list(branches['node-id-src']) + list(branches['node-id-dst'])
+    node_ids = list(branches["node-id-src"]) + list(branches["node-id-dst"])
     polylines = []
     for irow, row in branches_endpoint.iterrows():
-        polyline = np.array([(coordinates[0][i],coordinates[1][i]) for i in skan_skeleton.path(irow)])
+        polyline = np.array(
+            [[coordinates[0][i], coordinates[1][i]] for i in skan_skeleton.path(irow)]
+        )
         polyline = polyline[:, ::-1]  # same (x, y) order as phenomenal
 
         # verify that all phm_leaf polylines are oriented the same way (phm_leaf insertion --> phm_leaf tip)
-        i = row['node-id-dst']
+        i = row["node-id-dst"]
         if node_ids.count(i) > 1:
             polyline = polyline[::-1]
 
@@ -70,7 +74,9 @@ def skeleton_branches(image, n_kernel=15, min_length=30):
     return polylines
 
 
-def compute_extension(polylines_phm, polylines_sk, seg_length=50., dist_threshold=30.):
+def compute_extension(
+    polylines_phm, polylines_sk, seg_length=50.0, dist_threshold=30.0
+):
     """
 
     Args:
@@ -90,13 +96,11 @@ def compute_extension(polylines_phm, polylines_sk, seg_length=50., dist_threshol
     res = dict.fromkeys(range(len(polylines_phm)), [])
 
     for pl_sk in polylines_sk:
-
         b_selected = 0
-        dist_min = float('inf')
+        dist_min = float("inf")
         selected_rank = -1
 
         for rank, pl_phm in enumerate(polylines_phm):
-
             # end segment of phenomenal polyline
             dists_to_end = np.linalg.norm(pl_phm - pl_phm[-1], axis=1)
             start = np.argmin(abs(dists_to_end - seg_length))
@@ -104,10 +108,10 @@ def compute_extension(polylines_phm, polylines_sk, seg_length=50., dist_threshol
 
             # corresponding sk segment
             d_a = np.linalg.norm(pl_sk - pl_phm_segment[0], axis=1)
-            a, min_a = np.argmin(d_a), np.min(d_a)
+            a = np.argmin(d_a)
             d_b = np.linalg.norm(pl_sk - pl_phm_segment[-1], axis=1)
-            b, min_b = np.argmin(d_b), np.min(d_b)
-            pl_sk_segment = pl_sk[a:(b + 1)]
+            b = np.argmin(d_b)
+            pl_sk_segment = pl_sk[a : (b + 1)]
 
             # distance between phm and sk segments
             with warnings.catch_warnings():
@@ -126,9 +130,9 @@ def compute_extension(polylines_phm, polylines_sk, seg_length=50., dist_threshol
             l2 = polyline_length(pl_sk[b_selected:])
             extension_factor = (l2 + l1) / l1
 
-            res[selected_rank] = res[selected_rank] + [(round(dist_min, 4),
-                                                        extension_factor,
-                                                        pl_sk[b_selected:])]
+            res[selected_rank] = res[selected_rank] + [
+                (round(dist_min, 4), extension_factor, pl_sk[b_selected:])
+            ]
 
     # verify that a phenomenal polyline has no more than 1 corresponding skeleton polyline.
     # And keep extension polylines in a list for optional display
@@ -141,7 +145,7 @@ def compute_extension(polylines_phm, polylines_sk, seg_length=50., dist_threshol
             extension_polylines.append(extension_pl)
         # several skeleton branch candidates
         elif len(res[k]) > 1:
-            d_min = float('inf')
+            d_min = float("inf")
             selected_pl = None
             for d, extension_factor, extension_pl in res[k]:
                 if d < d_min:
@@ -176,13 +180,14 @@ def leaf_extension(phm_seg, binaries, projections):
 
     # compute extension for each phenomenal phm_leaf and each camera angle. Regroup results in a dictionary.
 
-    polylines_phm = [phm_seg.get_leaf_order(k).real_longest_polyline()
-                     for k in range(1, 1 + phm_seg.get_number_of_leaf())]
+    polylines_phm = [
+        phm_seg.get_leaf_order(k).real_longest_polyline()
+        for k in range(1, 1 + phm_seg.get_number_of_leaf())
+    ]
     angles = binaries.keys()
 
     res = dict()
     for angle in angles:
-
         # 2D skeleton polylines
         polylines_sk = skeleton_branches(binaries[angle])
 
@@ -200,19 +205,20 @@ def leaf_extension(phm_seg, binaries, projections):
     # (if no skeleton segment was found for a given phenomenal phm_leaf, extension factor have a default value of 1.)
 
     for k in range(1, 1 + phm_seg.get_number_of_leaf()):
-
         leaf_ext = [res[a][k - 1] for a in angles if res[a][k - 1] is not None]
 
-        if phm_seg.get_leaf_order(k).info['pm_label'] == 'growing_leaf':
-            leaf_length = phm_seg.get_leaf_order(k).info['pm_length_with_speudo_stem']
+        if phm_seg.get_leaf_order(k).info["pm_label"] == "growing_leaf":
+            leaf_length = phm_seg.get_leaf_order(k).info["pm_length_with_speudo_stem"]
         else:
-            leaf_length = phm_seg.get_leaf_order(k).info['pm_length']
+            leaf_length = phm_seg.get_leaf_order(k).info["pm_length"]
 
         if not leaf_ext:
-            extension_factor = 1.
+            extension_factor = 1.0
         else:
             extension_factor = np.median(leaf_ext)
 
-        phm_seg.get_leaf_order(k).info['pm_length_extended'] = leaf_length * extension_factor
+        phm_seg.get_leaf_order(k).info["pm_length_extended"] = (
+            leaf_length * extension_factor
+        )
 
     return phm_seg
