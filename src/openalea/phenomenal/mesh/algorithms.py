65d4f0c98670996991e20fb6aaade78ab2d4c3b8
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2025-02-12 11:40:19 +0100

    [no ci] adding missing doc + unifying doc string style

--- src/openalea/phenomenal/mesh/algorithms.py
+++ src/openalea/phenomenal/mesh/algorithms.py
@@ -45,14 +45,14 @@ def meshing(image_3d, smoothing_iteration=0, reduction=0.0, verbose=False):
     reduce the number triangle.
 
     Firstly :
-        A marching cubes algorithm is apply to compute the polygonal mesh.
+        A marching cubes algorithm is applied to compute the polygonal mesh.
 
     Secondly :
-        A smoothing algorithm is apply according the number of iteration
+        A smoothing algorithm is applied according the number of iteration
         given
 
     Thirdly :
-        A mesh decimation algorithm is apply according the percentage of
+        A mesh decimation algorithm is applied according the percentage of
         reduction given.
 
     Parameters
@@ -331,9 +331,6 @@ def voxelization(vtk_poly_data, voxels_size=1):
     voxels_size : float, optional
         Size of the voxels
 
-    verbose : bool, optional
-        If True, print for some information of each part of the algorithms
-
     Returns
     -------
     vtk_image_data : vtkImageData
@@ -417,12 +414,20 @@ def voxelization(vtk_poly_data, voxels_size=1):
     return imgstenc.GetOutput()
 
 
-def mesh_surface_area(vertices, faces):
-    """Return the surface_area of a mesh
+def mesh_surface_area(vertices, faces) -> float:
+    """
+    Returns the surface_area of a mesh
 
-    :param vertices:
-    :param faces:
-    :return:
+    Parameters
+    ----------
+    vertices: list[tuple]
+        The vertices of the mesh.
+    faces: list[tuple]
+        The faces of the mesh
+    Returns
+    -------
+    area: float
+        Surface area of mesh
     """
     return skimage.measure.mesh_surface_area(vertices, faces)
 
--- src/openalea/phenomenal/mesh/formats.py
+++ src/openalea/phenomenal/mesh/formats.py
@@ -28,8 +28,26 @@ __all__ = [
 
 def write_vertices_faces_to_ply_file(
     filename, vertices, faces, vertices_colors=None, faces_colors=None
-):
-    """Write methods to save vertices and faces in ply format."""
+) -> None:
+    """
+    Write methods to save vertices and faces in ply format.
+
+    Parameters
+    ----------
+    filename: str
+        The name of the file to write to. Should be ending with .ply
+    vertices: list[tuple]
+        An array of vertex positions.
+    faces: list[tuple]
+        An array of faces id.
+    vertices_colors: list[tuple] (optional)
+        An array of RGB color for each vertex.
+    faces_colors: list[tuple] (optional)
+        An array of RGB color for each face.
+    Returns
+    -------
+    None
+    """
 
     vtk_poly_data = from_vertices_faces_to_vtk_poly_data(
         vertices, faces, vertices_colors=vertices_colors, faces_colors=faces_colors
@@ -39,7 +57,19 @@ def write_vertices_faces_to_ply_file(
 
 
 def read_ply_to_vertices_faces(filename):
-    """Read ply file to vertices and faces"""
+    """
+    Read ply file to vertices and faces
+
+    Parameters
+    ----------
+    filename: str
+        The filename of a ply model
+
+    Returns
+    -------
+    mesh: tuple
+        A tuple with vertices, faces and color of the mesh.
+    """
 
     ply_reader = vtk.vtkPLYReader()
     ply_reader.SetFileName(filename)
@@ -64,7 +94,21 @@ def read_ply_to_vertices_faces(filename):
     return vertices, faces[:, 1:], colors
 
 
-def write_vtk_poly_data_to_ply_file(filename, vtk_poly_data):
+def write_vtk_poly_data_to_ply_file(filename, vtk_poly_data) -> None:
+    """
+    Write a vtk poly data to a ply file.
+
+    Parameters
+    ----------
+    filename: str
+        The name of the file to write to.
+    vtk_poly_data: vtkPolyData
+        The vtkPolyData object.
+
+    Returns
+    -------
+    None
+    """
     ply_writer = vtk.vtkPLYWriter()
     ply_writer.SetFileTypeToASCII()
     ply_writer.SetFileName(filename)
@@ -74,6 +118,18 @@ def write_vtk_poly_data_to_ply_file(filename, vtk_poly_data):
 
 
 def read_ply_to_vtk_poly_data(filename):
+    """
+    Reads the content of a ply file to a vtkPolyData object.
+
+    Parameters
+    ----------
+    filename: str
+        The name of the file to read.
+    Returns
+    -------
+    polydata: vtkPolyData
+        A poly data object
+    """
     ply_reader = vtk.vtkPLYReader()
     ply_reader.SetFileName(filename)
     ply_reader.Update()
