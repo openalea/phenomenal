6472440a6e71b5f31d389b32e5f33853d4df8a63
Christian Fournier <Christian.Fournier@inra.fr>
2025-05-28 13:40:08 +0200

    remove unneeded test image

--- test/test_display/test_display_image.py
+++ test/test_display/test_display_image.py
@@ -11,7 +11,7 @@ data_dir = test_subdir.parent / "data" / "plant_1"
 @pytest.mark.skip()
 def test_display_image():
     img = cv2.imread(data_dir / "bin" / "side" / "0.png")
-    img_col = cv2.imread(data_dir / "raw" / "side" / "0.png")
+    img_col = cv2.imread(data_dir / "bin" / "side" / "90.png")
     phm_display.show_image(img)
     phm_display.show_image(img_col)
     phm_display.show_images([img, img_col])
